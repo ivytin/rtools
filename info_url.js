ad051296fbaac0a64b63e0f22d8e84185ec6e9b9
ivytin <ivytin@gmail.com>
2015-08-18 12:21:20 +0800

    change the data format using in scrawling

--- info_url.js
+++ info_url.js
@@ -1,3 +1,17 @@
-Type, URL, Firmware, Hardware, DNS, Re_group
-TP-LINK TL-WR840N, /userRpm/Index.htm, var statusPara = new Array\(\n(.+?\n){5}"(.+?)", var statusPara = new Array\(\n(.+?\n){6}"(.+?)", var wanPara = new Array\(\n(.+?\n){11}"(.+?)", [2, 2, 2]
-DD_WRT, ${blank}, openAboutWindow.+?>(.+?)</a>, >Capture\(status_router.sys_model.+?\n(.+?)&nbsp;, ${blank}, [1, 1, 0] 
+{
+    "router_type": "TP-LINK TL-WR840N",
+    "info_url": "/userRpm/Index.htm",
+    "firmware": {"var statusPara = new Array.+?"(.+?)"": 1}
+    "hardware": {"var statusPara = new Array.+?".+?".+?"(.+?)"": 1}
+    "dns": {"var wanPara = new Array(.+?)"([\d\.]+? , [\d\.]+?)"": 2}
+    "also_support": ["WR702N", "WR720N", "WR740N", "WR741N", "WR743N", "WR750N", "WR802N", "WR840N", "WR841N", "WR841HP", "WR842N", "WR843N", "WR940N", "WR941N"]
+}
+
+{
+    "router_type": "DD_WRT",
+    "info_url": "",
+    "firmware": {"openAboutWindow.+?>(.+?)</a>": 1}
+    "hardware": {">Capture\(status_router.sys_model.+?\n(.+?)&nbsp;": 1}
+    "dns": {"": -1}
+    "also_support": []
+}
