79d290b0a0b4defdfbf57890593ca553d9ce1dce
ivytin <ivytin@gmail.com>
2015-08-19 15:08:38 +0800

    add support for dd-wrt

--- router_crawler.py
+++ router_crawler.py
@@ -3,7 +3,7 @@
 # @Author: tan
 # @Date:   2015-08-13 14:18:37
 # @Last Modified by:   tan
-# @Last Modified time: 2015-08-18 16:51:59
+# @Last Modified time: 2015-08-19 15:07:21
 
 import requests
 from requests.sessions import Session
@@ -11,6 +11,8 @@ from requests.auth import AuthBase
 import socket
 import re
 import base64
+from sqlite_helper import DataHelper
+from sqlite_helper import DataCondition
 
 class Error_port(Exception):
     """自定义的端口错误类"""
@@ -93,6 +95,7 @@ class RouterCrawler(object):
         self.router_name = name
         self.router_passwd = passwd
         self.router_addr = addr
+        self.db = DataHelper("testDB.db")
         if (0 < port and port < 65432):
             self.router_port = port
         else:
@@ -102,117 +105,115 @@ class RouterCrawler(object):
         else:
             raise Error_addr
             #self.addr = addr
+        self.db.open()
 
-    def detialCrawl(self, fm_re, hm_re, dns_re, raw):
+    def reCrawl(self, fm_re, fm_index, hm_re, hm_index, dns_re, dns_index, raw):
         """进一步在抓取固件版本，硬件版本，DNS等信息，基于正则表达式匹配"""
         fm_version = ''
         hm_version = ''
         dns = ''
-
-        if (fm_re[0] != ''):
-            fm_pattern = re.compile(fm_re[0], re.I | re.S)
+        if (fm_re != ''):
+            fm_pattern = re.compile(fm_re, re.I | re.S)
             fm_match = fm_pattern.search(raw)
             if fm_match:
-                fm_version = fm_match.group(fm_re[1])
+                fm_version = fm_match.group(fm_index)
 
-        if (hm_re[0] != ''):
-            hm_pattern = re.compile(hm_re[0], re.I | re.S)
+        if (hm_re != ''):
+            hm_pattern = re.compile(hm_re, re.I | re.S)
             hm_match = hm_pattern.search(raw)
             if hm_match:
-                hm_version = hm_match.group(hm_re[1])
+                hm_version = hm_match.group(hm_index)
 
-        if (dns_re[0] != ''):
-            dns_pattern = re.compile(dns_re[0], re.I | re.S)
+        if (dns_re != ''):
+            dns_pattern = re.compile(dns_re, re.I | re.S)
             dns_match = dns_pattern.search(raw)
             if dns_match:
-                dns = dns_match.group(dns_re[1])
+                dns = dns_match.group(dns_index)
 
         return fm_version, hm_version, dns
 
     def typeRec(self, fingerprint, raw):
         """区分路由器品牌，决定抓取方式，目前支持TP-Link, D-Link, DD-WRT"""
-        tp_re = 'TP[\W]?LINK'
-        ddwrt_re = 'DD[\W]?WRT'
-        dlink_re = 'D[\W]?LINK'
+
+        router_res = [('DD-WRT', 'DD\W?WRT'), ('TP-LINK', 'TP\W?LINK'), ('D-LINK', 'D\W?LINK')]
         #print raw
-
         #注意，DD-WRT必须出现在TP-Link或其他品牌之前，因为DD-WRT中包含其他品牌型号，导致识别错误
-        ddwrt_pattern = re.compile(ddwrt_re, re.I)
-        match = ddwrt_pattern.search(fingerprint)
-        if match:
-            return self.ROUTER_BRAND['DD-WRT']
-        else:
-            match = ddwrt_pattern.search(raw)
+        fingerprint = fingerprint + raw
+        for m_re in router_res:
+            m_pattern = re.compile(m_re[1], re.I)
+            match = m_pattern.search(fingerprint)
             if match:
-                return self.ROUTER_BRAND['DD-WRT']
+                rs = self.db.table("MODULE_MATCH").select('ID', 'FINGERPRINT').where(DataCondition(("=", "AND"), TYPE_INDEX = self.ROUTER_BRAND[m_re[0]]), DataCondition((">", "AND"), WEIGHT = 0)).fetchall()
+                for r in rs:
+                    r_pattern = re.compile(r[1])
+                    match = r_pattern.search(fingerprint)
+                    if match:
+                        return r[0]
+                r = self.db.table("MODULE_MATCH").select('ID').where(DataCondition(("=", "AND"), TYPE_INDEX = self.ROUTER_BRAND[m_re[0]]), DataCondition(("=", "AND"), WEIGHT = 0)).fetchone()
+                return r[0]
 
-        tp_pattern = re.compile(tp_re, re.I)
-        match = tp_pattern.search(fingerprint)
-        if match:
-            return self.ROUTER_BRAND['TP-LINK']
-        else:
-            match = tp_pattern.search(raw)
-            if match:
-                return self.ROUTER_BRAND['TP-LINK']
-
-        dlink_pattern = re.compile(dlink_re, re.I)
-        match = dlink_pattern.search(fingerprint)
-        if match:
-            return self.ROUTER_BRAND['D-LINK']
-        else:
-            match = dlink_pattern.search(raw)
-            if match:
-                return self.ROUTER_BRAND['D-LINK']
-
-        return self.ROUTER_BRAND['UNKNOW']
+        return -1
         #return self.ROUTER_BRAND['TP-LINK']
 
-    def tplinkCrawl(self, s, router_info, url):
-        cookie = base64.b64encode(self.router_name + ':' + self.router_passwd)
-        self.header['Cookie'] = 'Authorization=Basic ' + cookie
-        #self.header['Cookie'] = 'tLargeScreenP=1; subType=pcSub; Authorization=Basic YWRtaW46MTIzNDU2'
+    def infoCrawl(self, s, r, url, router_info):
+        #'INFO_URL', 'FIREWARE', 'FIREWARE_INDEX', 'HARDWARE', 'HARDWARE_INDEX', 'DNS', 'DNS_INDEX', 'AUTH_COOKIE', 'OTHER_COOKIE', 'REFERER'
+        info_url = r[0]
+        fm_re  = r[1]
+        fm_index = r[2]
+        hm_re = r[3]
+        hm_index = r[4]
+        dns_re = r[5]
+        dns_index = r[6]
+        auth_cookie = r[7]
+        other_cookie = r[8]
+        referer = r[9]
+
+        auth_cookie += base64.b64encode(self.router_name + ':' + self.router_passwd)
+        self.header['Cookie'] = other_cookie + auth_cookie
+        if referer == 'url':
+            self.header['Referer'] = url
+        else:
+            self.header['Referer'] = ''
+
         for x in xrange(3):
             try:
-                r = s.get(url + '/userRpm/StatusRpm.htm', auth = (self.router_name, self.router_passwd) , timeout = 5, allow_redirects = True, headers = self.header)
+                url += info_url
+                r = s.get(url, auth = (self.router_name, self.router_passwd) , timeout = 3, allow_redirects = True, headers = self.header)
                 break
             except Exception, e:
                 router_info['status'] = 'crwaling timeout'
                 return
+
         if (r.status_code == 401):
-            print 'Wrong username/passwd!'
+            router_info['status'] = 'Wrong username/passwd!'
         else:
+            router_id = router_info
             router_info['username'] = self.router_name
             router_info['passwd'] = self.router_passwd
             router_info['status'] = 'success'
-
-            #fm_re = [r'var statusPara = new Array\(\n(.+?\n){5}"(.+?)"', 2]
-            fm_re = [r'var statusPara.+?"(.+?)"', 1]
-            hm_re = [r'var statusPara.+?".+?".+?"(.+?)"', 1]
-            dns_re = [r'var wanPara(.+?)"([\d\.]+? , [\d\.]+?)"', 2]
-            print r.content
-            fm, hm, dns = self.detialCrawl(fm_re, hm_re, dns_re, r.content)
+            fm, hm, dns = self.reCrawl(fm_re, fm_index, hm_re, hm_index, dns_re, dns_index, r.content)
             router_info['fm_version'] = fm
             router_info['hm_version'] = hm
             router_info['dns'] = dns
+            return router_info
 
-    def ddwrtCrawl(self, s, router_info, url):
-        r = s.get(url, timeout = 3, allow_redirects = True, headers = self.header)
-        """DD-WRT"""
-        fm_re = [r'openAboutWindow.+?>(.+?)</a>', 1]
-        hm_re = [r'>Capture\(status_router.sys_model.+?\n(.+?)&nbsp;', 1]
-        dns_re = [r'', 0]
+    # def ddwrtCrawl(self, s, router_info, url):
+    #     r = s.get(url, timeout = 3, allow_redirects = True, headers = self.header)
+    #     """DD-WRT"""
+    #     fm_re = [r'openAboutWindow.+?>(.+?)</a>', 1]
+    #     hm_re = [r'>Capture\(status_router.sys_model.+?\n(.+?)&nbsp;', 1]
+    #     dns_re = [r'', 0]
 
-        fm, hm, dns = self.detialCrawl(fm_re, hm_re, dns_re, r.content)
-        router_info['fm_version'] = fm
-        router_info['hm_version'] = hm
-        router_info['dns'] = dns        
+    #     fm, hm, dns = self.reCrawl(fm_re, hm_re, dns_re, r.content)
+    #     router_info['fm_version'] = fm
+    #     router_info['hm_version'] = hm
+    #     router_info['dns'] = dns        
 
 
     def crawl(self, type = ROUTER_TYPE['BASE_AUTH']):
         #构造路由器地址
         url = 'http://' + self.router_addr + ':' + str(self.router_port)
         print url
-        self.header['Referer'] = url
         router_info = {
                 'url': url,
                 'status': '',
@@ -226,7 +227,7 @@ class RouterCrawler(object):
                 }
         s = Session()
         try:
-            r = s.get(url, timeout = 3, allow_redirects = True, headers = self.header)
+            r = s.get(url, timeout = 3, allow_redirects = True)
         except Exception, e:
             #print self.router_addr + ': Timeout!'
             print e
@@ -244,16 +245,27 @@ class RouterCrawler(object):
 
             #利用返回头部中的server和www-authenticate字段对路由器类型进行基本识别，再采取相应的抓取方法进行抓取
             fingerprint = router_info['router_server'] + ' ' + router_info['router_realm']
-            if (fingerprint != ''):
-                router_type = self.typeRec(fingerprint, r.content)
-                #print router_type
-                if router_type == self.ROUTER_BRAND['TP-LINK']:
-                    self.tplinkCrawl(s, router_info, url)
-                elif router_type == self.ROUTER_BRAND['DD-WRT']:
-                    self.ddwrtCrawl(s, router_info, url) 
+            #print r.content
+            if (fingerprint != '' or r.content != ''):
+                router_id = self.typeRec(fingerprint, r.content)
+                if router_id >= 0:
+                    r = self.db.table("INFO_MATCH").select('INFO_URL', 
+                                                      'FIREWARE', 'FIREWARE_INDEX', 
+                                                      'HARDWARE', 'HARDWARE_INDEX', 
+                                                      'DNS', 'DNS_INDEX', 
+                                                      'AUTH_COOKIE', 'OTHER_COOKIE',
+                                                      'REFERER'
+                                                      ).where(DataCondition(("=", "AND"), ID = router_id)).fetchone()
+                    if r == []:
+                        router_info['status'] = 'unsupport module'
+                    else:
+                        router_info = self.infoCrawl(s, r, url, router_info)
+                else:
+                    router_info['status'] = "can't recognize this module"
+            else:
+                router_info['status'] = "no fingerprint"
         finally:
             return router_info
-            pass
             #print r.content
 
 if __name__ == '__main__':
