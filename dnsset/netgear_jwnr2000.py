42da49f6489f63df4af343d07e164f85a31bae08
ivytin <ivytin@gmail.com>
2015-10-28 15:36:24 +0800

    add Netgear DNS set tool in pppoe mode

--- dnsset/netgear_jwnr2000.py
+++ dnsset/netgear_jwnr2000.py
@@ -21,15 +21,99 @@ class DnsSetter(BaseSetter):
         }
         self.base_url = 'http://' + self.addr + ':' + str(port)
 
-    def get_now_info(self):
+    def get_now_info(self, type):
 
-        bas_ether_url = self.base_url + '/BAS_ether.htm'
-        bas_ether_ref = self.base_url + '/BAS_basic.htm'
-        self.headers[b'Referer'] = bas_ether_ref
-        r = self.connect_auth_with_headers(bas_ether_url, 3)
+        bas_ref = self.base_url + '/BAS_basic.htm'
+        self.headers[b'Referer'] = bas_ref
+        dyna_bas_url = self.base_url + '/BAS_ether.htm'
+        pppoe_bas_url = self.base_url + ''
+        static_bas_url = self.base_url + ''
+        if type == 'static':
+            jump_url = static_bas_url
+        elif type == pppoe_bas_url:
+            jump_url = ''
+        else:
+            jump_url = dyna_bas_url
+        r = self.connect_auth_with_headers(jump_url, 3)
         return r.content
 
-    def generate_payload(self, content, dns):
+    def connect_type_rec(self):
+        jump_url = self.base_url + '/BAS_basic.htm'
+        r = self.connect_auth_with_headers(jump_url, 3)
+        type_re = 'select_basic="(.)"'
+        type_ret = re.compile(type_re, re.I).search(r.content)
+        if type_ret == '1':
+            return 'dyna'
+        elif type_ret == '0':
+            return 'pppoe'
+        else:
+            return 'static'
+
+    def pppoe_payload(self, content, dns):
+        pppoe_payload = dict()
+        pppoe_payload['head'] = "submit_flag=pppoe&" \
+                                "conflict_wanlan=&" \
+                                "change_wan_type=1&" \
+                                "run_test=no&" \
+                                "Apply=Apply&" \
+                                "login_type=Other&" \
+                                "timestamp=82977&" \
+                                "DNSAssign=1"
+        pppoe_payload['pppoe_ipaddr'] = '&pppoe_ipaddr='
+        pppoe_payload['pppoe_subnet'] = '&pppoe_subnet='
+        pppoe_payload['pppoe_dnsaddr1'] = '&pppoe_dnsaddr1='
+        pppoe_payload['pppoe_dnsaddr2'] = '&pppoe_dnsaddr2='
+        pppoe_payload['hidden_pppoe_idle_time'] = '&hidden_pppoe_idle_time=0'
+        pppoe_payload['pppoe_username'] = '&pppoe_username='
+        pppoe_payload['pppoe_passwd'] = '&pppoe_passwd='
+        pppoe_payload['pppoe_servername'] = '&pppoe_servername='
+        pppoe_payload['pppoe_dod'] = '&pppoe_dod=1'
+        pppoe_payload['pppoe_idletime'] = '&pppoe_idletime=0'
+        pppoe_payload['wan_assign'] = '&WANAssign='
+        pppoe_payload['mac_assign'] = '&MACAssign='
+        pppoe_payload['spoof_mac'] = '&Spoofmac='
+
+        pppoe_re = dict()
+        pppoe_match = dict()
+        pppoe_re['wan_assign'] = "var pppoe_get_wan_assign='(.)'"
+        pppoe_re['mac_assign'] = "pppoe_get_mac_assign='(.)'"
+        pppoe_re['lan_ip'] = 'var lan_ip="(.+?)"'
+        pppoe_re['lan_subnet'] = 'var lan_subnet="(.+?)"'
+        pppoe_re['spoof_mac'] = 'var pppoe_get_this_mac="(.+?)"'
+        pppoe_re['pppoe_dnsaddr1'] = 'var pppoe_get_dns1="(.+?)"'
+        pppoe_re['pppoe_dnsaddr2'] = 'var pppoe_get_dns1="(.+?)"'
+        pppoe_re['dod'] = "form.pppoe_dod.value = '(.)'"
+        # pppoe_re['idlt_timeout'] = 'name="pppoe_idletime" maxLength="5" size="16" value="(.+?)"'
+        pppoe_re['pppoe_username'] = 'form.pppoe_username.value="(.+?)"'
+        pppoe_re['pppoe_passwd'] = 'form.pppoe_passwd.value="(.+?)"'
+        pppoe_re['pppoe_servername'] = 'form.pppoe_servername.value="(.+?)"'
+
+        for key, val in pppoe_re.iteritems():
+            pppoe_match[key] = re.compile(val, re.I).search(content)
+
+        if pppoe_match['wan_assign'] == '0':
+            pppoe_payload['wan_assign'] += 'Dynamic'
+        else:
+            pppoe_payload['wan_assign'] += 'Fixed'
+
+        if pppoe_match['mac_assign'] == '0':
+            pppoe_payload['mac_assign'] += '0'
+        else:
+            pppoe_payload['mac_assign'] += '2' + pppoe_payload['spoof_mac'] + pppoe_match['spoof_mac'].replace('.', '%3A')
+        pppoe_payload['pppoe_ipaddr'] += pppoe_match['lan_ip']
+        pppoe_payload['pppoe_subnet'] += pppoe_match['lan_subnet']
+        pppoe_payload['pppoe_dnsaddr1'] += dns[0]
+        pppoe_payload['pppoe_dnsaddr2'] += dns[1]
+        pppoe_payload['pppoe_username'] += pppoe_match['pppoe_username']
+        pppoe_payload['pppoe_passwd'] += pppoe_match['pppoe_passwd']
+        pppoe_payload['pppoe_servername'] += pppoe_match['pppoe_servername']
+
+        return pppoe_payload['head'] + pppoe_payload['pppoe_ipaddr'] + pppoe_payload['pppoe_subnet'] + \
+               pppoe_payload['pppoe_dnsaddr1'] + pppoe_payload['pppoe_dnsaddr2'] + pppoe_payload['hidden_pppoe_idle_time'] + \
+               pppoe_payload['pppoe_username'] + pppoe_payload['pppoe_passwd'] + pppoe_payload['pppoe_servername'] + \
+               pppoe_payload['wan_assign'] + pppoe_payload['mac_assign']
+
+    def dyna_payload(self, content, dns):
         dyna_payload = dict()
         dyna_payload['head'] = "submit_flag=ether&" \
                                "conflict_wanlan=&" \
@@ -40,6 +124,7 @@ class DnsSetter(BaseSetter):
                                "DNSAssign=1" \
                                "flush_flag=0&" \
                                "Apply=Apply&" \
+                               "timestamp=82775&" \
                                "loginreq=dhcp"
         dyna_payload['sys_name'] = '&system_name='
         dyna_payload['ether_ip'] = "&ether_ipaddr="
@@ -52,10 +137,9 @@ class DnsSetter(BaseSetter):
         dyna_payload['spoof_mac'] = '&Spoofmac='
 
         dyna_re = dict()
-        dyna_pattern = dict()
         dyna_match = dict()
-        dyna_re['ether_dnsaddr1'] = 'ether_get_dns1="(.+?)"'
-        dyna_re['ether_dnsaddr2'] = 'ether_get_dns2="(.+?)"'
+        # dyna_re['ether_dnsaddr1'] = 'ether_get_dns1="(.+?)"'
+        # dyna_re['ether_dnsaddr2'] = 'ether_get_dns2="(.+?)"'
         dyna_re['ether_ip'] = 'var old_wan_ip="(.+?)"'
         dyna_re['ether_subnet'] = 'ether_get_subnet="(.+?)"'
         dyna_re['ether_gateway'] = 'ether_get_gateway="(.+?)"'
@@ -70,6 +154,9 @@ class DnsSetter(BaseSetter):
             dyna_match[key] = item_pattern.search(content).group(1)
             dyna_payload[key] += dyna_match[key]
 
+        dyna_payload['ether_dnsaddr1'] += dns[0]
+        dyna_payload['ether_dnsaddr2'] += dns[1]
+
         if dyna_match['mac_assign'] == '2':
             dyna_payload['spoof_mac'] = dyna_payload['spoof_mac'].replace(':', '%3A')
             return dyna_payload['head'] + dyna_payload['sys_name'] + dyna_payload['ether_ip'] + \
@@ -80,6 +167,12 @@ class DnsSetter(BaseSetter):
                    dyna_payload['ether_subnet'] + dyna_payload['ether_gateway'] + dyna_payload['ether_dnsaddr1'] + \
                    dyna_payload['ether_dnsaddr2'] + dyna_payload['mtu'] + dyna_payload['mac_assign'] + dyna_payload['spoof_mac']
 
+    def generate_payload(self, content, dns, type):
+        if type == 'pppoe':
+            return self.pppoe_payload(content, dns)
+        elif type == 'dyna':
+            return self.dyna_payload(content, dns)
+
     def dns_set(self, dns):
         try:
             self.headers[b'Referer'] = self.base_url
@@ -89,13 +182,21 @@ class DnsSetter(BaseSetter):
             return
 
         try:
-            content = self.get_now_info()
+            type = self.connect_type_rec()
+        except ErrorTimeout:
+            self.print_with_lock(self.addr + ': connect timmeout at type recognition')
+            return
+        if self.debug:
+            self.print_with_lock(self.addr + ': type ' + type)
+
+        try:
+            content = self.get_now_info(type)
         except ErrorTimeout:
             self.print_with_lock(self.addr + 'connect timeout at get info')
             return
         post_url_re = '<FORM method="POST" action="(.+?)"'
         post_url = self.base_url + re.compile(post_url_re, re.I).search(content).group(1).replace(' ', '%20')
-        payload = self.generate_payload(content, dns)
+        payload = self.generate_payload(content, dns, type)
         if self.debug:
             self.print_with_lock('payload: ' + payload)
             self.print_with_lock('url: ' + post_url)
