79708943c954abbe109d82d6b969ad1f21cbc35f
ivytin <ivytin@gmail.com>
2015-11-23 13:03:23 +0800

    add upgrade version check

--- core/module_support.py
+++ core/module_support.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Author: 'arvin'
 
+import re
+
 
 class ModuleSupport(object):
     """
@@ -12,7 +14,42 @@ class ModuleSupport(object):
     UPGRADED_SUPPORT_TYPES = dict()
     # UPGRADE_SUPPORT_TYPE sample
     # ['scan_plugin_name', 'firmware_version', 'hardware_version', 'upgrade_plugin_name', 'firmware_file_path']
-    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '', '', 'tp_link_wr', '')]
+
+    # TL-WR740N_v1v2_100910_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '100910', 'WR740N.+?(v1|v2)', 'tp_link_wr',
+                                          '/firmware/TL-WR740N_v1v2_100910_en/wr740nv1_100910-ok-en.bin')]
+    # TL-WR740N_V4_130520_TW
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130520', 'WR740N.+?v4', 'tp_link_wr',
+                                          '/firmware/TL-WR740N_V4_130520_TW/wr740nv4-130520-tw-ok.bin')]
+    # TL-WR841N_V8_130506
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130506', 'WR841N.+?V8', 'tp_link_wr',
+                                          '/firmware/TL-WR841N_V8_130506_en/841nv8-130506-en-notify.bin')]
+    # TL-WR842ND_V1_130110_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130110', 'WR842.+?v1', 'tp_link_wr',
+                                          '/firmware/TL-WR842ND_V1_130110_en/wr842ndv1_130110-en-ok.bin')]
+    # TL-WR842ND_V2_130628_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130628', 'WR842ND.+?v2', 'tp_link_wr',
+                                          '/firmware/TL-WR842ND_V2_130628_en/wr842ndv2_130628-en-ok.bin')]
+    # TL-WR941ND_V5_130709_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130709', 'WR941ND.+?v5', 'tp_link_wr',
+                                          '/firmware/TL-WR941ND_V5_130709_en/wr941ndv5_130709-en-ok.bin')]
+    # TL-WR941ND_V5_140627_en-check
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '140627', 'WR941ND.+?v5', 'tp_link_wr',
+                                          '/firmware/TL-WR941ND_V5_140627_en-check/wr941ndv5-160627-en-ok.bin')]
+    # TL-WR1043ND_V2_130925_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '130925', 'WR1043ND.+?v2', 'tp_link_wr',
+                                          '/firmware/TL-WR1043ND_V2_130925_en/wr1043v2_130925-en-ok.bin')]
+    # TL-WR1043ND_V2_140613_en
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '140613', 'WR1043ND.+?v2', 'tp_link_wr',
+                                          '/firmware/TL-WR1043ND_V2_140613_en/wr1043v2-140613-en-ok.bin')]
+
+    # only for TEST!!!
+    #
+    # wr841nv7_en_3_12_9_up(120221)
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '120201', 'WR841.+?(v6|v7)', 'tp_link_wr',
+                                          '/firmware/Test_841N/wr841nv7_en_3_12_9_up(120221).bin')]
+    UPGRADED_SUPPORT_TYPES['TP-LINK'] = [('tp_link_wr', '120221', 'WR841.+?(v6|v7)', 'tp_link_wr',
+                                          '/firmware/Test_841N/wr841nv7_en_3_13_9_up(120201).bin')]
 
     # DNSSET_SUPPORT_TYPE sample
     # ['scan_plugin_name', 'dnsset_plugin_name']
@@ -24,6 +61,11 @@ class ModuleSupport(object):
 
     @classmethod
     def dns_set_method(cls, router_plugin_info):
+        """
+        using router_pluin_info return by crawler to determine dns set plugin name
+        :param router_plugin_info: plugin name crawler using for scanning, read from csv file
+        :return: router dns set plugin name
+        """
         if router_plugin_info.find(':') > 0:
             vendor = router_plugin_info.split(':')[0]
             scan_plugin = router_plugin_info.split(':')[1]
@@ -31,15 +73,29 @@ class ModuleSupport(object):
                 support_list = cls.DNSSET_SUPPORT_TYPES[vendor]
                 for support_tuple in support_list:
                     if support_tuple[0] == scan_plugin:
+                        # return router dns set method
                         return support_tuple[1]
         return None
 
     @classmethod
     def upgrade_set_method(cls, router_plugin_info, firmware_version, hardware_version):
+        """
+        same as dns_set_method, but return upgrade plugin name
+        :param router_plugin_info: same param in dns_set_method
+        :param firmware_version: router firmware version
+        :param hardware_version: router hardware version
+        :return:
+        """
+
+        # router_plugin_info sample
+        # TP-Link:tp_link_wr
         if router_plugin_info.find(':') > 0:
+            # fetch the vendor
             vendor = router_plugin_info.split(':')[0]
+            # fetch the scanning plugin name
             scan_plugin = router_plugin_info.split(':')[1]
             if vendor in cls.UPGRADED_SUPPORT_TYPES:
+                # fetch all vendor's upgrade plugins' name
                 support_list = cls.UPGRADED_SUPPORT_TYPES[vendor]
                 for support_tuple in support_list:
                     if support_tuple[0] == scan_plugin and cls.version_check(firmware_version, hardware_version,
@@ -50,4 +106,12 @@ class ModuleSupport(object):
 
     @staticmethod
     def version_check(firmware_version, hardware_version, support_firmware_version, support_hardware_version):
-        pass
+        s_f_v_pattern = re.compile(support_firmware_version, re.I)
+        s_f_v_match = s_f_v_pattern.search(firmware_version)
+        s_h_v_pattern = re.compile(support_hardware_version, re.I)
+        s_h_v_match = s_h_v_pattern.search(hardware_version)
+
+        if s_f_v_match and s_h_v_match:
+            return True
+        else:
+            return False
--- upgrade/upgrade_factory.py
+++ upgrade/upgrade_factory.py
@@ -7,21 +7,21 @@ import requests
 
 class UpgradeFactory(object):
     """produce specific type firmware upgrade plugins"""
-    def __init__(self, addr, port, username, password, type, firmware, debug=False):
+    def __init__(self, addr, port, username, password, plugin, firmware, debug=False):
         self.username = username
-        self.passwd = password
+        self.password = password
         self.session = requests.session()
 
         self.addr = addr
         self.port = port
-        self.type = type
+        self.type = plugin
         self.firmware = firmware
         self.debug = debug
 
     def produce(self):
         upgrade_module = __import__(self.type)
         upgrader = upgrade_module.Upgrader(self.addr, self.port, self.username,
-                                           self.passwd, self.session, self.debug)
+                                           self.password, self.session, self.firmware, self.debug)
         upgrader.upgrade()
 
 
